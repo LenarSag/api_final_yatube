4ad910bfcf981e918db4dff327e3677d00ca1d9a
LenarSag <saglenar@gmail.com>
2024-04-17 09:24:02 +0300

    first review

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from rest_framework.exceptions import NotFound
 
 from posts.models import Post
 
@@ -25,5 +26,5 @@ class PostExistsAndAuthorOrReadOnly(AuthorOrReadOnly):
     def has_object_permission(self, request, view, obj):
         post = Post.objects.filter(pk=view.kwargs.get("post_id")).first()
         if post is None:
-            return False
+            raise NotFound
         return super().has_object_permission(request, view, obj)
